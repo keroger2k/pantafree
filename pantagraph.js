397b90716d4e347e0d9eb5dfc6f4f65256ced530
Kyle Rogers <kyle.rogers@gmail.com>
2013-11-11 21:49:01 -0600

    fixed bug with sites not having modal prompted

--- pantagraph.js
+++ pantagraph.js
@@ -1,23 +1,36 @@
 (function PantaFree() {
-	var exposeMask = document.getElementById('exposeMask');
+	var 
+	EXPOSE_MASK_CONST = "exposeMask",
+	PAYWALL_CONT = "paywall-code",
+	exposeMask = document.getElementById(EXPOSE_MASK_CONST), 
+	MAX_TRIES = 5;
 
 	if(exposeMask) exposeMask.remove();
 
 	findPayWall();
 
 	function findPayWall() {
-		var paywall = document.getElementById('paywall-code');
+		var paywall = document.getElementById(PAYWALL_CONT);
 		if(paywall) {
-			if(console) {
-				console.log('Found annoying modal.  Removing.');
-			}
+			consoleLog('Found annoying modal.  Removing.');
 			paywall.remove();
 			findPayWall = undefined;
 		} else {
-			if(console) {
-				console.log("Did not find paywall model.  Trying again...");
+			MAX_TRIES--;
+			consoleLog("Did not find paywall model.  Trying " + MAX_TRIES + " more times.");
+			if(MAX_TRIES === 0) {
+				consoleLog("Didn't find paywall; assuming not being prompted.");
+				findPayWall = undefined;
+			} else {
+				setTimeout(findPayWall, 1000);
 			}
-			setTimeout(findPayWall, 100);
 		}
 	}
+
+	function consoleLog(message) {
+		if(console) {
+			console.log(message);
+		}
+	}
+
 })();
